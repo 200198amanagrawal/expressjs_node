8a3c38b1a190e2b56733f343736999e1933ed364
Aman Agrawal <amanagrawal2098@gmail.com>
2020-02-06 23:16:02 +0530

    the url params

--- main.js
+++ main.js
@@ -9,4 +9,10 @@ app.get("/",(req,res)=>{
 app.get("/user/:id?/book/:bookid?",(req,res)=>{
     res.send("Hello users with id=>"+req.params.id+" and book id=>"+req.params.bookid);//? makes it optional for params call it with http://localhost:3000/user/3/bookid/4
 })
+/***
+ * another example where we are showing the flight status and making it optional.
+ */
+app.get("/flight/:from?.:to?",(req,res)=>{
+    res.send("Flight status from:"+req.params.from+" to:"+req.params.to);
+})
 app.listen(3000);
